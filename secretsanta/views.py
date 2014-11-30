164906cb449e3970871db973a438fb18c8e70c4e
dangerdak <dakshina.scott@gmail.com>
2014-11-30 11:12:18 +0000

    Inital commit

---
+++ secretsanta/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from secretsanta.forms import UserForm, UserProfileForm
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
