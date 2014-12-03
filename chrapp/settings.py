1d4e311fc31fe0a45f6aaf8bab460cc93b5f0ef3
dangerdak <dakshina.scott@gmail.com>
2014-12-03 21:44:14 +0000

    Start styling

--- chrapp/settings.py
+++ chrapp/settings.py
@@ -83,7 +83,6 @@ TEMPLATE_LOADERS = (
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -100,5 +99,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_URL = '/static/'
