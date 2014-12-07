e82fa1a4fa315fd68d480a30148c851be50bf276
dangerdak <dakshina.scott@gmail.com>
2014-12-07 21:41:57 +0000

    Env variables for smtp

--- chrapp/settings/prod.py
+++ chrapp/settings/prod.py
@@ -30,16 +30,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # A tuple that lists people who get code error notifications.
 ADMINS = (('Dak', get_env_variable('ADMIN_EMAIL')),)
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_USER = get_env_variable('SMTP_USER')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = get_env_variable('SMTP_PASSWORD')
 EMAIL_PORT = 587
 
 INSTALLED_APPS += (
