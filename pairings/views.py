9f5ff0087e7ac31b862dcd39be973eb5fc722b80
dangerdak <dakshina.scott@gmail.com>
2014-12-07 11:57:27 +0000

    Convert between profile and invitation ids

--- pairings/views.py
+++ pairings/views.py
@@ -2,7 +2,7 @@ from django.core import serializers
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from profiles.models import Profile, Membership
+from profiles.models import Profile, Membership, Invitation
 
 
 def assign_pairs(self, slug):
@@ -17,6 +17,8 @@ def assign_pairs(self, slug):
     for member in serializers.deserialize('python', data):
         member.save()
 
+    convert(data, invitation_to_profile)
+
     i = 0
     # Matching algorithm
     for member in data:
@@ -25,7 +27,32 @@ def assign_pairs(self, slug):
     for member in serializers.deserialize('python', data):
         member.save()
 
+    convert(data, profile_to_invitation)
     return HttpResponseRedirect(reverse('home'))
+
+def convert(data, fcn):
+    """Convert data by applying function to all avoids and prefers"""
+    # Avoids and prefers are saved as invitations instead of profiles
+    # so must be converted before and after assignments are made
+    for member in data:
+        for avoid in member['fields']['avoid']:
+            avoid = fcn(avoid)
+        for prefer in member['fields']['prefer']:
+            prefer = fcn(prefer)
+        member.save()
+
+def profile_to_invitation(profile_id):
+    """Return invitation id corresponding to given profile id."""
+    profile = Profile.objects.get(id=profile_id)
+    invitation = Invitation.objects.get(to_name=profile.user.username)
+    return invitation.id
+
+
+def invitation_to_profile(invitation_id):
+    """Return profile id corresponding to given invitaion id."""
+    invitation = Invitation.objects.get(id=invitation_id)
+    profile = Profile.objects.get(user__username=invitation.to_name)
+    return profile.id
 
 # def assign_pairs(request):
 #     profiles = Profile.objects.all()
